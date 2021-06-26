735b0466042ed7ec5d5ac6b4b655a48ced48ad30
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-26 21:25:21 +0530

    Update delete.py

--- bot/modules/delete.py
+++ bot/modules/delete.py
@@ -15,7 +15,7 @@ def deletefile(update, context):
 		link = msg_args[1]
 		LOGGER.info(msg_args[1])
 	except IndexError:
-		msg = 'Send a link along with command'
+		msg = 'Send a link along with command 😏'
 
 	if msg == '' : 
 		drive = gdriveTools.GoogleDriveHelper()
