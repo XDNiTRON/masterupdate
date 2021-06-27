484deb6601b1f827edc4822bb93764745323bbc7
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-27 12:30:03 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -154,7 +154,7 @@ class MirrorListener(listeners.MirrorListeners):
                 msg += f'\n<b>├• SubFolders : </b><code>{folders}</code>'
                 msg += f'\n<b>╰• Files : </b><code>{files}</code>'
             else:
-                msg += f'\n\n<b>─• Type : </b><code>{typ}</code>'
+                msg += f'\n\n<b>➣ Type : </b><code>{typ}</code>'
             buttons = button_build.ButtonMaker()
             if SHORTENER is not None and SHORTENER_API is not None:
                 surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={link}&format=text').text
