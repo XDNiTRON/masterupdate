3a66e91dfe977c0627567b33cb939d0c25a9b4e5
xbeet <84825812+xbeet@users.noreply.github.com>
2021-07-02 16:52:17 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -347,7 +347,7 @@ class GoogleDriveHelper:
             if meta.get("mimeType") == self.__G_DRIVE_DIR_MIME_TYPE:
                 dir_id = self.create_directory(meta.get('name'), parent_id)
                 result = self.cloneFolder(meta.get('name'), meta.get('name'), meta.get('id'), dir_id)
-                msg += f'<b>📂FileName : </b><code>{meta.get("name")}</code>\n\n<b>🧰TotalSize : </b><code>{get_readable_file_size(self.transferred_size)}</code>'
+                msg += f'<b>📂FileName : </b><code>{meta.get("name")}</code>\n\n<b>🧰 TotalSize : </b><code>{get_readable_file_size(self.transferred_size)}</code>'
                 msg += f'\n\n<b>╭• Type : </b><code>Folder</code>'
                 msg += f'\n<b>├• SubFolders : </b><code>{self.total_folders}</code>'
                 msg += f'\n<b>╰• Files : </b><code>{self.total_files}</code>'
@@ -355,17 +355,17 @@ class GoogleDriveHelper:
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={durl}&format=text').text
-                    buttons.buildbutton("🔰 G-Drive Link 🔰", surl)
+                    buttons.buildbutton("🔰 G-DRIVE LINK 🔰", surl)
                 else:
-                    buttons.buildbutton("🔰 G-Drive Link 🔰", durl)
+                    buttons.buildbutton("🔰 G-DRIVE LINK 🔰", durl)
                 if INDEX_URL is not None:
                     url_path = requests.utils.quote(f'{meta.get("name")}')
                     url = f'{INDEX_URL}/{url_path}/'
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={url}&format=text').text
-                        buttons.buildbutton("🏓 Index Link 🏓", siurl)
+                        buttons.buildbutton("🏓 INDEX LINK 🏓", siurl)
                     else:
-                        buttons.buildbutton("🏓 Index Link 🏓", url)
+                        buttons.buildbutton("🏓 INDEX LINK 🏓", url)
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
                     buttons.buildbutton(f"{BUTTON_FOUR_NAME}", f"{BUTTON_FOUR_URL}")
                 if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
@@ -379,15 +379,15 @@ class GoogleDriveHelper:
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={durl}&format=text').text
-                    buttons.buildbutton("🔰 G-Drive Link 🔰", surl)
+                    buttons.buildbutton("🔰 G-DRIVE LINK 🔰", surl)
                 else:
-                    buttons.buildbutton("🔰 G-Drive Link 🔰", durl)
+                    buttons.buildbutton("🔰 G-DRIVE LINK 🔰", durl)
                 try:
                     typeee = file.get('mimeType')
                 except:
                     typeee = 'File' 
                 try:
-                    msg += f'\n\n<b>🧰TotalSize : </b><code>{get_readable_file_size(int(meta.get("size")))}</code>'
+                    msg += f'\n\n<b>🧰 TotalSize : </b><code>{get_readable_file_size(int(meta.get("size")))}</code>'
                     msg += f'\n\n<b>➣ Type : </b><code>{typeee}</code>'
                 except TypeError:
                     pass
@@ -398,13 +398,13 @@ class GoogleDriveHelper:
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={url}&format=text').text
                         siurls = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={urls}&format=text').text
-                        buttons.buildbutton("🏓 Index Link 🏓", siurl)
+                        buttons.buildbutton("🏓 INDEX LINK 🏓", siurl)
                         if VIEW_LINK:
-                            buttons.buildbutton("🎥 Stream Link 🎥", siurls)
+                            buttons.buildbutton("🎟 STREAM LINK 🎟", siurls)
                     else:
-                        buttons.buildbutton("🏓 Index Link 🏓", url)
+                        buttons.buildbutton("🏓 INDEX LINK 🏓", url)
                         if VIEW_LINK:
-                            buttons.buildbutton("🎥 Stream Link 🎥", urls)
+                            buttons.buildbutton("🎟 STREAM LINK 🎟", urls)
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
                     buttons.buildbutton(f"{BUTTON_FOUR_NAME}", f"{BUTTON_FOUR_URL}")
                 if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
@@ -655,7 +655,7 @@ class GoogleDriveHelper:
             if drive_file['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
                 self.gDrive_directory(**drive_file)
                 msg += f'<b>📂FileName : </b><code>{name}</code>'
-                msg += f'\n\n<b>🧰TotalSize : </b><code>{get_readable_file_size(self.total_bytes)}</code>'
+                msg += f'\n\n<b>🧰 TotalSize : </b><code>{get_readable_file_size(self.total_bytes)}</code>'
                 msg += f'\n\n<b>╭• Type : </b><code>Folder</code>'
                 msg += f'\n<b>├• SubFolders : </b><code>{self.total_folders}</code>'
                 msg += f'\n<b>╰• Files : </b><code>{self.total_files}</code>'
@@ -668,7 +668,7 @@ class GoogleDriveHelper:
                 try:
                     self.total_files += 1
                     self.gDrive_file(**drive_file)
-                    msg += f'\n\n<b>🧰TotalSize : </b><code>{get_readable_file_size(self.total_bytes)}</code>'
+                    msg += f'\n\n<b>🧰 TotalSize : </b><code>{get_readable_file_size(self.total_bytes)}</code>'
                     msg += f'\n\n<b>╭• Type : </b><code>{typee}</code>'
                     msg += f'\n<b>╰• Files : </b><code>{self.total_files}</code>'
                 except TypeError:
