a91f463eb0ea7450318177a2566d4e51bdb46a9b
xbeet <84825812+xbeet@users.noreply.github.com>
2021-07-02 16:07:30 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -21,7 +21,7 @@ def cloneNode(update, context):
                 LOGGER.info(f"Checking File/Folder if already in Drive...")
                 smsg, button = gd.drive_list(name)
                 if smsg:
-                    msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
+                    msg3 = "File/Folder is already available in Drive 😡\nHere are the search results 🥶"
                     sendMarkup(msg3, context.bot, update, button)
                     return
             if CLONE_LIMIT is not None:
@@ -29,7 +29,7 @@ def cloneNode(update, context):
                 limit = CLONE_LIMIT
                 limit = limit.split(' ', maxsplit=1)
                 limitint = int(limit[0])
-                msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(clonesize)}.'
+                msg2 = f'Failed, Copy limit is ➤ {CLONE_LIMIT}.\nYour File/Folder size is ➤ {get_readable_file_size(clonesize)}.'
                 if 'G' in limit[1] or 'g' in limit[1]:
                     if clonesize > limitint * 1024**3:
                         sendMessage(msg2, context.bot, update)
@@ -38,7 +38,7 @@ def cloneNode(update, context):
                     if clonesize > limitint * 1024**4:
                         sendMessage(msg2, context.bot, update)
                         return              
-        msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
+        msg = sendMessage(f"🌈 Copying 🌈 <code>{link}</code>", context.bot, update)
         result, button = gd.clone(link)
         deleteMessage(context.bot, msg)
         if button == "":
@@ -49,10 +49,10 @@ def cloneNode(update, context):
             else:
                 uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
             if uname is not None:
-                cc = f'\n\ncc: {uname}'
+                cc = f'\n\n🚨UpLoader: {uname}'
             sendMarkup(result + cc, context.bot, update, button)
     else:
-        sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
+        sendMessage('Provide G-Drive Shareable Link to Clone 😏', context.bot, update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(clone_handler)
