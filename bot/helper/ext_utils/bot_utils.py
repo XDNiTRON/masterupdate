80165a17f1fe8696c124fe35360fefc4398c2f0a
NiTRONDC1 <84825812+NiTRONDC@users.noreply.github.com>
2021-07-13 16:53:48 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -114,6 +114,7 @@ def get_readable_message():
                         f"\n<b>╰• 🛰 Peers :</b> {download.aria_download().connections}"
                 except:
                     pass
+                msg += f'\n<b>User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 msg += f"\n\n<b>💥ToStop ➤ </b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             msg += "\n\n"
